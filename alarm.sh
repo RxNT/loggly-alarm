e317632cc033ffa54d6724909b6c20d27871a698
Billy Romansky <wromansky@protonmail.com>
2019-01-24 09:43:01 -0500

    modified:   alarmConfig.sh

--- alarm.sh
+++ alarm.sh
@@ -14,7 +14,7 @@
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-source alertConfig.sh
+source alarmConfig.sh
 source credentials.sh
 
 #echo ""
